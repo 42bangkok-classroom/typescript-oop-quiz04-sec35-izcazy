436cd5f816b5f5aefb1ebf4790b4c1f04d9a07ae
izcazy <ice.izcazy@gmail.com>
2026-03-21 16:18:42 +0700

    p02.5

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Get('test')
-    getTest() {
-        return this.userService.test();}
+  @Get('test')
+  getTest() {
+    return this.userService.test();
+  }
 }
