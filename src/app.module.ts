f115f16b1e406f58b5820842c9da32fa6f8b2c21
izcazy <ice.izcazy@gmail.com>
2026-03-21 16:26:04 +0700

    p02.6

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
